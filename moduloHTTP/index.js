a03194653bc2cd925bc4bdc150494b8e59d6581f
Túlio Henrique <tuliohenriques@hotmail.com>
2020-07-31 23:51:42 -0300

    modulo HTTP
    modulo HTTP

--- moduloHTTP/index.js
+++ moduloHTTP/index.js
@@ -1,1 +1,12 @@
-//criar servidor por JS para receber requisicoes
+//criar servidor por JS para receber requisições
+import http from "http";
+
+http.createServer((req, res) => {
+    if((req.method === "GET") && (req.url === "/teste")) {
+        res.write("GET /teste executado com sucesso");
+    } else {
+        res.write("Hello World!");
+    }
+    res.statusCode = 200;
+    res.end();
+}).listen(8080);
